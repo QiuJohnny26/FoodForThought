a50805e2882cbada86c5ffdf446c8d38bbd6af0b
Johnny Qiu <qiujohnny26@hotmail.com>
2018-08-16 23:03:25 -0400

    JQuery/Scrolling Added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     
+/********** For Sticky Navigation *******************************************************************************************/
   $('.js--section-features').waypoint(function(direction){
     if (direction == "down"){
       $('nav').addClass('sticky')
@@ -12,7 +13,14 @@ $(document).ready(function(){
    offset:'10px;'
   });
 
-    
-    
+     
+/********** For Scrolling *******************************************************************************************/   
+$('.js--scroll-to-plans').click(function(){
+    $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},1000);
+});
+
+$('.js--scroll-to-start').click(function(){
+    $('html,body').animate({scrollTop:$('.js--section-features').offset().top},500);
+})
     
 });
